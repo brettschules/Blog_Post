cea12f8e0471f4b428fe91c2c996e97479e5279f
Brett Schules <brettschules@gmail.com>
2017-09-27 16:12:34 -0400

    Set up Relationship table for users to be followers and followed by other users, still have to work on active record error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,16 @@
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-  has_many :active_relationships, class_name: "Relationship",
-                                  foreign_key: "follower_id",
-                                  dependent: :destroy
+  has_many :active_relationships,  class_name:  "Relationship",
+                                   foreign_key: "follower_id",
+                                   dependent:   :destroy
+  has_many :passive_relationships, class_name:  "Relationship",
+                                   foreign_key: "followed_id",
+                                   dependent:   :destroy
+  has_many :following, through: :active_relationships,  source: :followed
+  # source allows to change followeds to following
+
+  has_many :followers, through: :passive_relationships
+
 
   has_secure_password
 
@@ -13,4 +21,20 @@ class User < ApplicationRecord
             uniqueness: {case_sensitive: false}
 
   validates :password, presence: true, length: {minimum: 6}, allow_nil: true
+  # Follows a user.
+  def follow(other_user)
+    following << other_user
+  end
+
+  # Unfollows a user.
+  def unfollow(other_user)
+    following.delete(other_user)
+  end
+
+  # Returns true if the current user is following the other user.
+  def following?(other_user)
+    following.include?(other_user)
+  end
+
+
 end
